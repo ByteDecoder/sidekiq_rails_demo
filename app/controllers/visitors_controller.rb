ea0fe0a9d673530ab7f4c28c7707f9c6396d89bd
Rodrigo Reyes <rreyes@canaryhealth.com>
2019-07-04 17:18:54 -0500

    Added some ajusts to the email controller

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,12 +3,10 @@ class VisitorsController < ApplicationController
   end
 
   def contact
-    h = JSON.generate({ 'name' => params[:name],
-                        'email' => params[:email],
-                        'message' => params[:message] })
-
-    # if instead of sidekiq I was just sending email from rails
-    VisitorMailer.contact_email(@name, @email, @message).deliver_later
+    VisitorMailer.contact_email(params[:name],
+                                params[:email],
+                                params[:message])
+                 .deliver_later
 
     redirect_to :root
   end
